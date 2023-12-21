c8305f80859db848e60ea2dbdad0f1bd9fb23eaf
rod773 <104315743+rod773@users.noreply.github.com>
2023-12-21 12:32:27 +0100

    index view

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({});
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/index.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+});
